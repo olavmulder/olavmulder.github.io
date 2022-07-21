22256168fc7e5d7bb262f4debdbf4d7fad895ecb
olavmulder <olavmulder02@gmail.com>
2022-07-21 13:49:53 +0200

    v1.00

--- calculate.js
+++ calculate.js
@@ -10,12 +10,12 @@ if (form.attachEvent) {
 }
 function CalculatePoints(){
     let sum;
-    sum = points60(form.elements['60m'].value);
-    sum += pointsHJ(form.elements['hoog'].value);
-    sum += pointsSP(form.elements['kogel'].value);
-    sum += pointsBal(form.elements['bal'].value);
-    sum += pointsLJ(form.elements['ver'].value);
-    sum += pointsEsta60(form.elements['esta'].value);
+    sum = con.points60(form.elements['60m'].value);
+    sum += con.pointsHJ(form.elements['hoog'].value);
+    sum += con.pointsSP(form.elements['kogel'].value);
+    sum += con.pointsBal(form.elements['bal'].value);
+    sum += con.pointsLJ(form.elements['ver'].value);
+    sum += con.pointsEsta60(form.elements['esta'].value);
     console.log(sum);
     return sum;
 }
